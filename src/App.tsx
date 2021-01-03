798e6987b128cf5f5c46a60397d4176e649d94a2
Adam Sallai <yadam1998@gmail.com>
2021-01-03 20:15:32 +0100

    add basic files and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MainPage from './components/main/MainPage';
+import VideoDetail from './components/details/VideoDetail';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/videos" component={VideoDetail} />
+      </Router>
     </div>
   );
 }
